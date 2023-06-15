cf5398d87cf8aa338d58baa574dce1a427bc09ce
fouadslh001 <salhanefouad@gmail.com>
2023-06-15 13:25:24 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - first print with betty with printf
+ * Return: the return is 0 that means its sucsese
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
