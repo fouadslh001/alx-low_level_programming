29590d2daacf60cd78e0e9fb81feec6297d03e22
fouadslh001 <salhanefouad@gmail.com>
2023-06-15 12:22:43 -0700

    add my first c programe

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * programe c that print "Programming is like building a multilingual puzzle" with only puts
+ */
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
+}	
