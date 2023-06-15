0c3702f059cfcb80b9e1223cea01874810bd41b1
fouadslh001 <salhanefouad@gmail.com>
2023-06-15 12:36:17 -0700

    fixbug

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,6 @@
 #include <stdio.h>
-
-/**
- * programe c that print "Programming is like building a multilingual puzzle" with only puts
- */
-
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
-	return 0;
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }	
