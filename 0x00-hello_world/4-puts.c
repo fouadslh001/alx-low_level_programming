eeb5ab8f298621badb49b0a2a251d6cd47448152
fouadslh001 <salhanefouad@gmail.com>
2023-06-15 13:18:01 -0700

    betty make me lost my minde

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  * main - first print with betty
- * return : the return is 0 that means its sucsese
+ * Return: the return is 0 that means its sucsese
  */
 int main(void)
 {
