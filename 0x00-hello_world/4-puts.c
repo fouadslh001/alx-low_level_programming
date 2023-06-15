b9d49c7b65b880ceab86de405010ba3bc6b2419f
fouadslh001 <salhanefouad@gmail.com>
2023-06-15 13:13:53 -0700

    fix final bug

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
+/**
+ * main - first print with betty
+ * return : the return is 0 that means its sucsese
+ */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
-}	
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
