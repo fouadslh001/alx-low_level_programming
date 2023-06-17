65a6facf5d4e5ed7e9b8017ea3f6d6fc4117a788
fouadslh001 <salhanefouad@gmail.com>
2023-06-17 11:43:39 -0700

    fix final buf betty

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,19 +7,21 @@
 int main(void)
 {
 	int x = 0;
+
 	while (x < 10)
 	{
-	putchar(x + '0');
-	if (x != 9)
+		putchar(x + '0');
+		if (x != 9)
 	{
-	putchar(',');
-	putchar(' ');
+		putchar(',');
+		putchar(' ');
 
 	}
-	x++;
+		x++;
 	}
 
 	putchar('\n');
+
 	return (0);
 
 
