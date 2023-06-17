b27da22c5327d6abb4374bedf2ab57e4a6a0f457
fouadslh001 <salhanefouad@gmail.com>
2023-06-17 10:37:32 -0700

    i diidit after long study

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: 0 means sucsses
+ */
+
+int main(void)
+{
+	int f = 1;
+	int l = 10;
+	char ff = 'a';
+	char ll = 'f';
+
+	while (f < l) 
+	{
+		putchar(f + '0');
+		f++;
+	}
+
+	while (ff <= ll) 
+	{
+		putchar(ff);
+		ff++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
