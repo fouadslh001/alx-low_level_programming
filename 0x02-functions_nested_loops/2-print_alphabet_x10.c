96aa22b1b7d9fb3442045d27b661049ec743da63
fouadslh001 <salhanefouad@gmail.com>
2023-06-20 13:29:42 -0700

    10times az

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - entry point for this programe
+ * Return: 0 mean sucssees
+ */
+
+void print_alphabet_x10(void)
+{
+	char i = 'a';
+	char x = 'z';
+	int rp = 0;
+
+	while (0 <= 10)
+	{
+		while (i <= x)
+		{
+			_putchar(i);
+			i++,
+		}
+
+	_putchar('\n');	
+	_putchar(i);
+	rp++;
+	i = 'a'
+	}
+	
+}
