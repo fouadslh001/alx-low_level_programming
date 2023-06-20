c5ea3d94f8816bcc89835461426d35209d875b82
fouadslh001 <salhanefouad@gmail.com>
2023-06-20 14:32:15 -0700

    f

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isalpha - entry point for this programe
+ * @c: ascii code
+ * Return: 0 mean sucssees
+ */
+
+int _isalpha(int c);
+{
+	if (c >= 97 && c <= 122 || c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
