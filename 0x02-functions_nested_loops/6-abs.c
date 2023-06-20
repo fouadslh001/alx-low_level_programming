1ec0a3650d8369c6a67844807f760c6684f437ff
fouadslh001 <salhanefouad@gmail.com>
2023-06-20 15:42:58 -0700

    fi

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _abs - entry point for this programe
+ * @i: the input number as ion integer
+ * Return:  absolute value
+ */
+
+int _abs(int)
+{
+	if (i >= 0)
+	{
+		return (i);
+	}
+	else
+	{
+		return (n * -1);
+	}
+	_putchar('\n');
+}
