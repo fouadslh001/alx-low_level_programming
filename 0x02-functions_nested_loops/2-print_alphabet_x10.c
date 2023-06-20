5fa8ea09a588b2883da42e365387f9aaeb3a0c40
fouadslh001 <salhanefouad@gmail.com>
2023-06-20 14:02:13 -0700

    f

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,21 +8,20 @@
 
 void print_alphabet_x10(void)
 {
-    char i = 'a';
-    char x = 'z';
-    int rp = 1;
+char i = 'a';
+char x = 'z';
+int rp = 1;
 
-    while (rp <= 10)
-    {
-	    while (i <= x)
-        {
-            _putchar(i);
-            i++;
-        }
-
-        _putchar('\n');
-        i = 'a';
-        rp++;
-    }
+while (rp <= 10)
+{
+while (i <= x)
+{
+_putchar(i);
+i++;
 }
 
+_putchar('\n');
+i = 'a';
+rp++;
+}
+}
