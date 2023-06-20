378a0aff28ff7ed095cb1a87854c196d500fb4f8
fouadslh001 <salhanefouad@gmail.com>
2023-06-20 16:25:37 -0700

    f

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - entry pooint
+ * @a: An integer a
+ * @b: An integer b
+ * Description: Function that adds two integers and returns the result
+ * Return: Result of add a and b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
