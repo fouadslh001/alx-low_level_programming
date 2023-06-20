6fd8a38179746342306dd122e9d0d19e9a0ad0cd
fouadslh001 <salhanefouad@gmail.com>
2023-06-20 15:28:59 -0700

    f

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_sign - entry point for this programe
+ * @c: ascii code
+ * Return: 0 mean sucssees
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar(45);
+		return (-1);
+	}
+	else (n == 0)
+	{
+		_putchar(48);
+		return (0);
+	}
+	_putchar('\n');
+}
