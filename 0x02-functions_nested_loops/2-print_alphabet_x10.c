dcdebcad42c99821408ae8cd7910dd9e4c3d95b3
fouadslh001 <salhanefouad@gmail.com>
2023-06-20 13:43:02 -0700

    fix

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -11,16 +11,16 @@ void print_alphabet_x10(void)
 	char x = 'z';
 	int rp = 0;
 
-	while (0 <= 10)
+	while (rp <= 10)
 	{
 	while (i <= x)
 	{
 		_putchar(i);
-		i++,
+		i++;
 	}
 
 	_putchar('\n');	
-	_putchar(i);
+	i = 'a';
 	rp++;
 	
 	}
