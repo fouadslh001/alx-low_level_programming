ea1df4f8c47be0fea4b3a735d68d705d139c4fb5
fouadslh001 <salhanefouad@gmail.com>
2023-06-20 12:03:41 -0700

    lewer alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ * Description: alphabet
+ * Return: 0 mean sucssees
+ */
+
+int main()
+{
+	char i = a;
+	char x = z;
+
+	while (i <= x )
+		{
+			_putchar(i);
+			i++;
+		}
+	_putchar('\n')
+
+	return (0);
+
+}
