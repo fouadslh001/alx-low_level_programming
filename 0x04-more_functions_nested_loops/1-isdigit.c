7f44e8872bc17cc4a6e7118b286654e32265b31c
fouadslh001 <salhanefouad@gmail.com>
2023-06-22 04:37:32 -0700

    f

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * int _isdigit - this entry point
+ * _isdigit - this entry point
  * @c: the caracter to print
  * Return: 0 means otherwise  and 1 means digits
  */
@@ -9,7 +9,11 @@
 int _isdigit(int c)
 {
 	if (c >= '0' && c <= '9')
+	{
 		return (1);
+	}
 	else
+	{
 		return (0);
+	}
 }
