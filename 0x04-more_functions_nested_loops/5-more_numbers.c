dddd9bca042d67adb4dd0fa287714f5ca6ec1c22
fouadslh001 <salhanefouad@gmail.com>
2023-06-22 07:18:07 -0700

    fix

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,11 +9,15 @@ void more_numbers(void)
 {
 	int i, j;
 
-	for (j = '0'; j <= '9'; j++)
+	for (j = 0; j < 10; j++)
 	{
-		for (i = '0'; i <= '14'; i++)
-			_putchar(i);
+		for (i = 0; i <= 14; i++)
+		{
+			if (i > 9)
+				_putchar(i / 10 + '0');
+
+			_putchar(i % 10 + '0');
+		}
 	}
-	_putchar(j);
 	_putchar('\n');
 }
