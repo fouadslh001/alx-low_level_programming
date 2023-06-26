232853ac122fd0297036402fe29e0d9406109c56
fouadslh001 <salhanefouad@gmail.com>
2023-06-26 13:47:39 -0700

    fix

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * rev_string - print reverse string fonction
+ * @s: character pointer
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	int len = 0, i = 0;
+	char aux;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+
+	while (i < len--)
+		aux = s[i];
+		s[i++] = s[len];
+		s[len] = aux;
+
+}
