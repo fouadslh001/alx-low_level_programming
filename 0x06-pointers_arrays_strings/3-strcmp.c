cb054485385e8f232d6a6be7ac83c1d25ca7ecee
fouadslh001 <salhanefouad@gmail.com>
2023-06-29 06:47:07 -0700

    f

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,11 +9,11 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	while (*s1 && (*s1 == *s2))
+	while (*s1 && *s2)
 	{
-		s1++;
-		s2++;
+		if (*s1 != *s2)
+			return (*s1 - *s2);
+		s1++, s2++;
 	}
-
-	return *(unsigned char*)s1 - *(unsigned char*)s2;
+	return (*s1 - *s2);
 }
