651a72e3e38675b2cd289a2f56e35a1cb8eb5dfe
fouadslh001 <salhanefouad@gmail.com>
2023-07-07 10:00:33 -0700

    lib

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,17 +7,33 @@
 int main(void)
 {
 	int x = 1;
+	int b = 14;
+	int c = 23;
+	int d = 34;
+	int e = 45;
+	int f = 56;
+	int g = 67;
+	int h = 78;
+	int k = 89;
 
-	while (x < 90)
+	while (x < 14 && b < 20 && c < 30 && d < 40 && e < 50 && f < 60 && g < 70 && h < 80 && k < 90)
 	{
-		printf("%02d", x);
-		if (x != 89)
+		printf("%02d, %02d, %02d, %02d, %02d, %02d, %02d, %02d, %02d", x, b, c, d, e, f, g, h, k);
 	{
 		putchar(',');
 		putchar(' ');
 
 	}
 		x++;
+		b++;
+		c++;
+		d++;
+		e++;
+		f++;
+		g++;
+		h++;
+		k++;
+
 	}
 
 	putchar('\n');
