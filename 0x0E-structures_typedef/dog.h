28131c12692178d972f797964b02e16472dfda96
fouadslh001 <salhanefouad@gmail.com>
2023-07-17 14:07:58 -0700

    f

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOUBLEGUARD
+#define DOUBLEGUARD
+
+/**
+ * struct dog - Define a new struct dog with name, age, owner
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of dog
+ **/
+
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+}
+
+#endif
