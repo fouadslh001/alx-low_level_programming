f632d0018b96f6f7b7c2653b8c7d37cac5a4e6a6
fouadslh001 <salhanefouad@gmail.com>
2023-08-01 03:52:38 -0700

    update

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**f
+ * pop_listint - function will dealate node list and returns itts contents
+ * @head: head of thes list
+ * Return: contents of thee head
+ */
+int pop_listint(listint_t **head)
+{
+	int i;
+	listint_t *current, *tmp;
+
+	if (head == NULL)
+		return (0);
+	tmp = current = *head;
+	if (*head)
+	{
+		i = current->n;
+		*head = current->next;
+		free(tmp);
+	}
+	else
+		i = 0;
+	return (i);
+}
