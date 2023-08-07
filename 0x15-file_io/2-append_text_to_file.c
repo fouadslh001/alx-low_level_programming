83531618a1afc07a284b8243ac2d85f8d705374d
fouadslh001 <salhanefouad@gmail.com>
2023-08-07 09:29:48 -0700

    f

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include "main.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+
+/**
+ * append_text_to_file -my function will  Appends text at the end of this file.
+ * the of standard output.
+ * @filename: Name of the file the file we want to append.
+ * @text_content: NULL terminated string, to write for the file.
+ * Return: 1 on success, -1 on failure or processe is not wor.
+ */
+
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd, res_write, lenght;
+
+	if (filename == NULL)
+		return (-1);
+	if (text_content != NULL)
+	{
+		fd = open(filename, O_WRONLY | O_APPEND);
+		if (fd == -1)
+			return (-1);
+		lenght = 0;
+		while (*(text_content + lenght) != '\0')
+			lenght++;
+		res_write = write(fd, text_content, lenght);
+		if (res_write == -1)
+		{
+			close(fd);
+			write(STDOUT_FILENO, "fails", 5);
+			return (-1);
+		}
+	}
+	close(fd);
+	return (1);
+}
