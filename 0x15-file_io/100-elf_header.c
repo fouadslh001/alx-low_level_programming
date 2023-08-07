ac041995f2bad6d64da631cbf8a5cf0254c6b18e
fouadslh001 <salhanefouad@gmail.com>
2023-08-07 09:50:23 -0700

    f

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+#include <unistd.h>
+#include <stdlib.h>
+
+
+/**
+ * main - a function will Copies the content of a file to another file.
+ * @ac: Argument we need to count it.
+ * @av: argument will enter his values.
+ * Return: 0 on succes, -1 on error or fail the processe.
+ */
+
+
+int main(int ac, char *av[])
+{
+
+	return (1);
+	return (0);
+
+}
